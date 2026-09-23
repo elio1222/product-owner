aa518b531f878d153f0a7299bf090932163c640f
eliorocha <e.rocha1204@gmail.com>
2026-09-23 14:54:34 -0400

    feat: backend flag to init subcommand

--- cli/init.py
+++ cli/init.py
@@ -2,10 +2,11 @@ from core.commands.init import initialize_po
 
 def register(sub):
     init = sub.add_parser("init", help="initialize po and configure settings")
+    init.add_argument("--project", type=str, help="project name")
     init.add_argument("--author", type=str, help="author")
     init.add_argument("--email", type=str, help="author")
-    init.add_argument("--project", type=str, help="project name")
     init.add_argument("--desc", type=str, help="description of project")
+    init.add_argument("--backend", type=str,choices=["sqlite","postgresql"], default="sqlite")
     init.add_argument("--force", action="store_true", help="overwrites any previous init command")
     init.add_argument("--no-git-check", action="store_true", help="ignore if git has been initialized")
 
@@ -13,6 +14,6 @@ def register(sub):
 
 
 def handle(args):
-    initialized = initialize_po(author=args.author, email=args.email, project=args.project, desc = args.desc, force=args.force, no_git_check=args.no_git_check)
+    initialized = initialize_po(author=args.author, email=args.email, project=args.project, desc = args.desc, backend = args.backend, force=args.force, no_git_check=args.no_git_check)
 
     print("successfully initialized")
